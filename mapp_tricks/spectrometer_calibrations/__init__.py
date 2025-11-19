8b3bc041172e2b5640deb2cc3485dae8c22b652c
LarsEggimann <lars.eggimann@gmail.com>
2025-11-19 15:16:37 +0100

    moving editing and stuff

--- mapp_tricks/spectrometer_calibrations/__init__.py
+++ mapp_tricks/spectrometer_calibrations/__init__.py
@@ -1,1 +1,2 @@
 from .HPGe.hpge_calib import HPGeCalibration, FitData
+from .x_ray.xray_calib import XRayCalibration
--- playground/playground.py
+++ playground/playground.py
@@ -1,2 +1,13 @@
-from mapp_tricks.film_analyzer import *
+from uncertainties import ufloat
+from mapp_tricks.spectrometer_calibrations import XRayCalibration
 
+calib = XRayCalibration(
+    level=1,
+    source_radius=1,
+)
+
+test = calib.evaluate_efficiency_at(
+    ufloat(10, 0.1)
+    )
+
+print(test)
