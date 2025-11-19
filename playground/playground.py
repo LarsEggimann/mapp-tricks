499c9a64af96caa3196c97469aba7c2bb262610e
LarsEggimann <lars.eggimann@gmail.com>
2025-11-19 15:44:20 +0100

    release

--- playground/playground.py
+++ playground/playground.py
@@ -3,11 +3,10 @@ from mapp_tricks.spectrometer_calibrations import XRayCalibration
 
 calib = XRayCalibration(
     level=1,
-    source_radius=1,
 )
 
 test = calib.evaluate_efficiency_at(
     ufloat(10, 0.1)
     )
 
-print(test)
+fig = calib.get_plot()
