fae0c01807b9db0ca9f249b74d4e482b2d7a83a5
LarsEggimann <lars.eggimann@gmail.com>
2026-07-01 12:06:04 +0200

    playground

--- playground/playground.py
+++ playground/playground.py
@@ -10,3 +10,20 @@ test = calib.evaluate_efficiency_at(
     )
 
 fig = calib.get_plot()
+
+
+from mapp_tricks.spectrometer_calibrations import HPGeCalibration
+
+calib = HPGeCalibration(level=1, with_aluminum_foil=True)
+
+e1 = 670.32
+e2 = 962.84
+
+eff1 = calib.evaluate_efficiency_at_energy(e1)
+eff2 = calib.evaluate_efficiency_at_energy(e2)
+
+print(f"Efficiency at {e1} keV: {eff1:uS}")
+print(f"Efficiency at {e2} keV: {eff2:uS}")
+
+calib.print_summary()
+calib.plot_fit()
